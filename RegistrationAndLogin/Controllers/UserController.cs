f91983ec86f4e49909ede8747f2acaadb463ae3b
ridvannamli <33039545+ridvannamli@users.noreply.github.com>
2018-06-21 16:15:18 +0300

    Complete login and registration system in ASP.NET MVC application

---
+++ RegistrationAndLogin/Controllers/UserController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationAndLogin.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        //Registration POST action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerified,ActivationCode")] User user)
+        {
+            bool Status = false;
+            string message = "";
+
+            //Model Validation
+            if (ModelState.IsValid)
+            {
+                #region //Email is already exist 
+
+                var isExist = IsEmailExist(user.EmailID);
+                if (isExist)
+                {
+                    ModelState.AddModelError("Email Exist", "Email is already exist");
+                    return View(user);
+                }
+
+                #endregion
+
+                #region Generate Activation Code
+                user.ActivationCode = Guid.NewGuid();
+                #endregion
+
+                #region Password Hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);  //
+                #endregion
+                user.IsEmailVerified = false;
+
+                #region Save to Database
+                using (RegistrationAndLoginDBEntities dc = new RegistrationAndLoginDBEntities())
+                {
+                    dc.Users.Add(user);
+                    dc.SaveChanges();
+
+                    //Send Email to User 
+                    SendVerificationLinkEmail(user.EmailID, user.ActivationCode.ToString());
+                    message = "Registration successfully done. Activation link " + "has been sent to your email id:" + user.EmailID;
+                    Status = true;
+                }
+                #endregion
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(user);
+        }
+                
+        //Verify Email
+
+        //Verify Email LINK
+
+
+        //Login
+
+
+        //Login POST 
+
+
+        //Logout
+
+        [NonAction]
+        public bool IsEmailExist(string emailID)
+        {
+            using (RegistrationAndLoginDBEntities dc = new RegistrationAndLoginDBEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string emilID, string activationCode)
+        {
+            var verifyUrl = "/User/VerifyAccount/" + activationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("ridvannamli@gmail.com", "Rıdvan Namlı"); //Replace with your email and name-surname
+            var toEmail = new MailAddress(emilID);
+            var fromEmailPassword = "********";  //Replace with actual password , Password is necessary absolutely...
+            string subject = "Your account is succesfully created!";
+
+            string body = "<br/><br/>Your account is" + "successfully created. Please click on the below link to verify your account" +
+                "<br/><br/><a href='" + link + "'>" + link + "</a>";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+
+        }
+
+    }
+}
